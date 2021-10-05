0f2346308c6fb97df2ae358e170d2fc0c85bb8a6
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-10-05 20:28:29 -0300

    Update exclusão.php

--- exclusão.php
+++ exclusão.php
@@ -1,1 +1,12 @@
-
+<?php
+	include "conexao.php";
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+	
+  $sql = "DELETE FROM usuario WHERE NOME = $nome AND EMAIL = $email; 
+  
+	mysqli_query($connect, $sql) or die(error());
+	$response = array("success" => true);
+	echo json_encode($response);
+?>
